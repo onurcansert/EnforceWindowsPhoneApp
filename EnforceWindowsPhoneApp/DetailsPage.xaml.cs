77bb091a56c9b4ca0c2d346fcbf1440b857411cc
Onur Can <onurcansert@gmail.com>
2013-10-04 15:59:59 +0300

    Detay Sayfası Eklendi
    Detay Sayfası ana hatlarıyla oluşturuldu. Harita ve yorumlar şu anda
    dinamik olarak çalışmıyor, statik veriler üzerinden görüntüleniyor.

--- EnforceWindowsPhoneApp/DetailsPage.xaml.cs
+++ EnforceWindowsPhoneApp/DetailsPage.xaml.cs
@@ -11,6 +11,11 @@ using Microsoft.Phone.Shell;
 using EnforceWindowsPhoneApp.Utils;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Device.Location;
+using Windows.Devices.Geolocation;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace EnforceWindowsPhoneApp
 {
@@ -28,6 +33,41 @@ namespace EnforceWindowsPhoneApp
             ComplaintTitle.Text = tmpComplaint.Title;
             ComplaintImage.Source = tmpComplaint.Source;
             ComplaintCategory.Text = tmpComplaint.Category;
+            ComplaintAddress.Text = tmpComplaint.Address;
+
+            List<Comment> ComplaintCommentList = new List<Comment>();
+            Comment c1 = new Comment();
+            c1.Id = "Onur";
+            c1.Text = "Acayip güzel bir problem bu bayıldım ;)";
+
+            Comment c2 = new Comment();
+            c2.Id = "Onur";
+            c2.Text = "Acayip güzel bir problem bu bayıldım ;)";
+
+            ComplaintCommentList.Add(c1);
+            ComplaintCommentList.Add(c2);
+
+            ComplaintComments.ItemsSource = ComplaintCommentList;
+
+            /*MapOverlay overlay = new MapOverlay
+            {
+                GeoCoordinate = ComplaintMap.Center,
+                Content = new Ellipse
+                {
+                    Fill = new SolidColorBrush(Colors.Red),
+                    Width = 40,
+                    Height = 40
+                }
+            };
+            MapLayer layer = new MapLayer();
+            layer.Add(overlay);
+
+            ComplaintMap.Layers.Add(layer);*/
+
+            //Geolocator locator = new Geolocator();            
+            //ComplaintMap.Center = new GeoCoordinate(39.916123, 32.8539379);
+            //ComplaintMap.SetView(new GeoCoordinate(39.916123D, 32.8539379D), 17D);
+            //ComplaintMap.ZoomLevel = 3;
         }
     }
 }
--- EnforceWindowsPhoneApp/Utils/Comment.cs
+++ EnforceWindowsPhoneApp/Utils/Comment.cs
@@ -8,73 +8,13 @@ namespace EnforceWindowsPhoneApp.Utils
 {
     class Comment
     {
-        private String id;
-        private User author;
-        private String text;
+        public String Id { get; set; }
+        public User Author { get; set; }
+        public String Text { get; set; }
 
-        private DateTime date;
+        public DateTime Date { get; set; }
 
-        private int like;
-        private int dislike;
-
-        public void setId(String id)
-        {
-            this.id = id;
-        }
-
-        public String getId()
-        {
-            return id;
-        }
-
-        public void setText(String text)
-        {
-            this.text = text;
-        }
-
-        public String getText()
-        {
-            return text;
-        }
-
-        public void setDate(DateTime date)
-        {
-            this.date = date;
-        }
-
-        public DateTime getDate()
-        {
-            return date;
-        }
-
-        public void setLikeCount(int like)
-        {
-            this.like = like;
-        }
-
-        public int getLikeCount()
-        {
-            return like;
-        }
-
-        public void setDislikeCount(int dislike)
-        {
-            this.dislike = dislike;
-        }
-
-        public int getDislikeCount()
-        {
-            return dislike;
-        }
-
-        public void setAuthor(User author)
-        {
-            this.author = author;
-        }
-
-        public User getAuthor()
-        {
-            return author;
-        }
+        public int Like { get; set; }
+        public int Dislike { get; set; }
     }
 }
